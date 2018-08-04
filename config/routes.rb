bce42636538c5426d03266499a0fb1673853305d
TaeyeobKim <hwj20215@likelion.org>
2018-08-05 01:44:48 +0900

    taeyeob

--- config/routes.rb
+++ config/routes.rb
@@ -11,6 +11,8 @@ Rails.application.routes.draw do
 
   get 'sajus/payment'
 
+  get 'sajus/qna'
+
   post 'sajus/newreview'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
